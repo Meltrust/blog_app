8cb974a06908909b8ae362195a63b7e859d8bec1
Chukwuma Obasi <ccobasi8@gmail.com>
2021-03-17 21:03:15 +0100

    (Add) resources to route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root "articles#index"
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
 end
