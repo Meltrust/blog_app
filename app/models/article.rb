3723baacd240ad20a4d70b264d32ef0a6c91fdf1
meltrust <original.mtapia@outlook.com>
2021-03-18 09:38:32 -0600

    finish to point 8.2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
-  end
-  
+  has_many :comments
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'articles#index'
 
-  root "articles#index"
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
-
+  resources :articles do
+    resources :comments
+  end
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
